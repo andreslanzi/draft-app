1b5a2855e697ac0aab833bfedd710cef1a722cc4
andreslanzi <aandreslanzi@gmail.com>
2024-08-28 15:12:50 -0300

    asd

--- src/Steps/Step3.tsx
+++ src/Steps/Step3.tsx
@@ -66,7 +66,7 @@ const Step3 = () => {
 				>
 					Atrás
 				</button>
-				<button
+				{/* <button
 					className="w-[300px] h-10  select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 px-4 rounded-lg bg-cyan-700 hover:bg-cyan-600 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none  "
 					type="button"
 					onClick={async () => {
@@ -74,7 +74,7 @@ const Step3 = () => {
 					}}
 				>
 					Continuar
-				</button>
+				</button> */}
 			</div>
 		</>
 	);
