cb426c003fd0ab13ab0860ce785459fea775a513
andreslanzi <aandreslanzi@gmail.com>
2024-08-28 14:20:39 -0300

    removing logs

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -85,8 +85,8 @@ export const StoreProvider = ({ children }: StoreProps) => {
 		teams: {},
 	});
 
-	console.log({ boardData });
-	console.log({ groups });
+	// console.log({ boardData });
+	// console.log({ groups });
 
 	const getPlayersQuantity = () => players.length;
 
